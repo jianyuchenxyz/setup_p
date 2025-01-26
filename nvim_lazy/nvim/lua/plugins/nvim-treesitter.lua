92f342342169824c8461c9feb0628ca2eef465a2
jianyuchenxyz <jianyu.chen.xyz@proton.me>
2025-01-26 21:18:28 +0800

    add complete Neovim configs using LazyVim

---
+++ nvim_lazy/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,37 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "bash",
+                "css",
+                "diff",
+                "dockerfile",
+                "go",
+                "gomod",
+                "gosum",
+                "gotmpl",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "luadoc",
+                "make",
+                "markdown_inline",
+                "nix",
+                "toml",
+                "vim",
+                "vimdoc",
+                "vimdoc",
+                "xml",
+                "yaml",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
