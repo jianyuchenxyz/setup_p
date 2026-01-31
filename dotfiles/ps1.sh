8e6b92ed84fa4cdca6f1bf02918758a94fc6c958
jianyuchenxyz <jianyu.chen.xyz@proton.me>
2026-01-31 14:25:56 +0800

    ps1.sh

--- dotfiles/ps1.sh
+++ dotfiles/ps1.sh
@@ -13,41 +13,42 @@ MAGENTA=$(tput setaf 5)
   WHITE=$(tput setaf 7)
 #-----------------------
 
-current_time() {
-    printf '%s%s%s' "${BLUE}" "$(date +%H:%M:%S)" "${RESET}"
-}
-
-trailing_two_dirs() {
-    if [ $(pwd) = "${HOME}" ]; then
-        DIR_STR='~'
+_prompt() {
+    _exit_code=${?}
+    if [ ${_exit_code} -eq 0 ]; then
+        _status_color="${GREEN}"
+        _status_mark='✔'
     else
-        DIR_STR="$(pwd | awk -F '/' '{print $(NF-1), $(NF)}' | sed 's/ /\//')"
+        _status_color="${RED}"
+        _status_mark='✘'
     fi
 
-    OLD_IFS=${IFS}
-    IFS=""
-    printf ' | %s📂 %s%s' "${CYAN}" "${DIR_STR}" "${RESET}"
-    IFS=${OLD_IFS}
-}
+    _last_command_status=$(printf ' | %s%s %d%s' "${_status_color}" "${_status_mark}" "${_exit_code}" "${RESET}")
 
-last_command_status() {
-    EXIT_CODE=${?}
-    if [ ${EXIT_CODE} -eq 0 ]; then
-        printf ' | %s✔ %d%s' "${GREEN}" "${EXIT_CODE}" "${RESET}"
-        return
+    _time=$(printf '%s%s%s' "${BLUE}" "$(date +%H:%M:%S)" "${RESET}")
+
+    _pwd="$(pwd)"
+    if [ "${_pwd}" = "${HOME}" ]; then
+        _dirstr='~'
+    else
+        # Return the path without the last component.
+        _tmpdir="${_pwd%/*}"
+
+        # Remove the longest match followed by a "/", and concatenate
+        # with the last component.
+        _dirstr="${_tmpdir##*/}/${_pwd##*/}"
     fi
 
-    printf ' | %s✘ %d%s' "${RED}" "${EXIT_CODE}" "${RESET}"
-}
+    _dir=$(printf ' | %s📂 %s%s' "${CYAN}" "${_dirstr}" "${RESET}")
 
-parse_git_branch() {
-    git rev-parse 2> /dev/null
-    if [ ${?} -eq 0 ]; then
-        OLD_IFS=${IFS}
-        IFS=""
-        printf ' | %s⎇ %s%s' "${MAGENTA}" "$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/')" "${RESET}"
-        IFS=${OLD_IFS}
+    _parse_branch=$(git symbolic-ref -q --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
+    if [ ! -z "${_parse_branch}" ]; then
+        _git_branch=$(printf ' | %s⎇ %s%s' "${MAGENTA}" "${_parse_branch}" "${RESET}")
     fi
+
+    _prompt_trail='∟❱❱'
+
+    printf '\n[ %s%s%s%s ]\n %s ' "${_time}" "${_last_command_status}" "${_dir}" "${_git_branch}" "${_prompt_trail}"
 }
 
-export PS1="\n[ "${BLUE}"\t"${RESET}"\$(last_command_status)\$(trailing_two_dirs)\$(parse_git_branch) ]\n ∟❱❱ "
+export PS1="\$(_prompt)"
