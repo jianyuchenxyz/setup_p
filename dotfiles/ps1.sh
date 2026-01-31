ccd4a86b5b470bce281577d8db7bbb384d0bb1be
jianyuchenxyz <jianyu.chen.xyz@proton.me>
2026-01-31 14:38:34 +0800

    ps1.sh

--- dotfiles/ps1.sh
+++ dotfiles/ps1.sh
@@ -1,31 +1,31 @@
 # Terminal Prompt - should be POSIX-compliant.
 
-#----- ANSI Colors -----
-  RESET=$(tput sgr0)
-   BOLD=$(tput bold)
+#------- ANSI Colors -------
+  _c_reset=$(tput sgr0)
+   _c_bold=$(tput bold)
 
-    RED=$(tput setaf 1)
-  GREEN=$(tput setaf 2)
- YELLOW=$(tput setaf 3)
-   BLUE=$(tput setaf 4)
-MAGENTA=$(tput setaf 5)
-   CYAN=$(tput setaf 6)
-  WHITE=$(tput setaf 7)
-#-----------------------
+    _c_red=$(tput setaf 1)
+  _c_green=$(tput setaf 2)
+ _c_yellow=$(tput setaf 3)
+   _c_blue=$(tput setaf 4)
+_c_magenta=$(tput setaf 5)
+   _c_cyan=$(tput setaf 6)
+  _c_white=$(tput setaf 7)
+#---------------------------
 
 _prompt() {
     _exit_code=${?}
     if [ ${_exit_code} -eq 0 ]; then
-        _status_color="${GREEN}"
+        _status_color="${_c_green}"
         _status_mark='✔'
     else
-        _status_color="${RED}"
+        _status_color="${_c_red}"
         _status_mark='✘'
     fi
 
-    _last_command_status=$(printf ' | %s%s %d%s' "${_status_color}" "${_status_mark}" "${_exit_code}" "${RESET}")
+    _last_command_status=$(printf ' | %s%s %d%s' "${_status_color}" "${_status_mark}" "${_exit_code}" "${_c_reset}")
 
-    _time=$(printf '%s%s%s' "${BLUE}" "$(date +%H:%M:%S)" "${RESET}")
+    _time=$(printf '%s%s%s' "${_c_blue}" "$(date +%H:%M:%S)" "${_c_reset}")
 
     _pwd="$(pwd)"
     if [ "${_pwd}" = "${HOME}" ]; then
@@ -39,11 +39,11 @@ _prompt() {
         _dirstr="${_tmpdir##*/}/${_pwd##*/}"
     fi
 
-    _dir=$(printf ' | %s📂 %s%s' "${CYAN}" "${_dirstr}" "${RESET}")
+    _dir=$(printf ' | %s📂 %s%s' "${_c_cyan}" "${_dirstr}" "${_c_reset}")
 
     _parse_branch=$(git symbolic-ref -q --short HEAD 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
     if [ ! -z "${_parse_branch}" ]; then
-        _git_branch=$(printf ' | %s⎇ %s%s' "${MAGENTA}" "${_parse_branch}" "${RESET}")
+        _git_branch=$(printf ' | %s⎇ %s%s' "${_c_magenta}" "${_parse_branch}" "${_c_reset}")
     fi
 
     _prompt_trail='∟❱❱'
